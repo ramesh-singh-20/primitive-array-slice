92cd6e4883f80fe5bea2835f769d6fc16a9a1ffc
Ramesh Singh <singh.ramesh.eee@gmail.com>
2021-03-24 12:06:14 -0400

    Added code to slice primitive array with Streams  method.

--- src/main/java/com/alphacoder/array/util/ArraySlicer.java
+++ src/main/java/com/alphacoder/array/util/ArraySlicer.java
@@ -1,6 +1,7 @@
 package com.alphacoder.array.util;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class ArraySlicer {
     private ArraySlicer(){
@@ -8,8 +9,10 @@ public class ArraySlicer {
     }
 
     public static int[] sliceArray(int[] arr, int startIndex, int lastIndex){
-        int [] slice= Arrays.copyOfRange(arr, startIndex, lastIndex);
+        return Arrays.copyOfRange(arr, startIndex, lastIndex);
+    }
 
-        return slice;
+    public static int[] sliceArrayUsingStream(int [] arr, int startIndex, int lastIndex){
+        return IntStream.range(startIndex, lastIndex).map(i-> arr[i]).toArray();
     }
 }
--- src/test/unit/groovy/com/alphacoder/array/util/ArraySlicerTest.groovy
+++ src/test/unit/groovy/com/alphacoder/array/util/ArraySlicerTest.groovy
@@ -19,4 +19,19 @@ class ArraySlicerTest extends Specification{
         result[3]==3
     }
 
+    def 'Test ArraySlicerUsingStream'(){
+        given:
+        def arr= new int[] {2,3,4,5,7,8,3,4,6}
+
+        when:
+        def result= ArraySlicer.sliceArrayUsingStream(arr, 3, 7)
+
+        then:
+        result.length== 4
+        result[0]== 5
+        result[1]==7
+        result[2]==8
+        result[3]==3
+    }
+
 }
